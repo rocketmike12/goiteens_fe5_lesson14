c539b6acdba95c88035151fce8e813e872d6e2cd
rocketmike12 <mg.korop@gmail.com>
2025-09-21 14:08:12 +0300

    added github actions deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/goiteens_fe5_lesson12/"
+	base: "/goiteens_fe5_lesson14/"
 });
